8879b8b4fd76d5a7882171ab328ab0111d15d5e7
phongux <ungxuanphong@gmail.com>
2020-10-21 22:59:13 +0700

    update

--- test2/test.py
+++ test2/test.py
@@ -6,26 +6,26 @@ lib_path = os.path.abspath(os.path.join('config'))
 print(lib_path)
 sys.path.append(lib_path)
 
-# import config.conn
-#
-# def application(environment, start_response):
-#     from webob import Response
-#     con = config.conn.Connect()
-#     accounts = con.get_acount()
-#
-#     page = f"""
-#         <!doctype html>
-#         <html>
-#             <head>
-#                 <title> Login </title>
-#             </head>
-#         <body>
-#             <p> test %s</p>
-#         </body>
-#     </html>"""%str(accounts)
-#
-#     response = Response(body=page,
-#                         content_type="text/html",
-#                         charset="utf8",
-#                         status="200 OK")
-#     return response(environment, start_response)
+import config.conn
+
+def application(environment, start_response):
+    from webob import Response
+    con = config.conn.Connect()
+    accounts = con.get_acount()
+
+    page = f"""
+        <!doctype html>
+        <html>
+            <head>
+                <title> Login </title>
+            </head>
+        <body>
+            <p> test %s test</p>
+        </body>
+    </html>"""%str(accounts)
+
+    response = Response(body=page,
+                        content_type="text/html",
+                        charset="utf8",
+                        status="200 OK")
+    return response(environment, start_response)
